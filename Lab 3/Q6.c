da4ea4c62644ff18672be96e65c2933d21346cb0
Maarij <127932628+Fortres7@users.noreply.github.com>
2024-09-08 12:50:01 +0500

    Create Q6.c

---
+++ Lab 3/Q6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float x1 = 5, y1 = 4;
+    float x2 = 3, y2 = 2;
+    float slope;
+
+    slope = (y2 - y1) / (x2 - x1);
+
+    printf("The slope of the line passing through points (5, 4) and (3, 2) is: %.3f\n", slope);
+
+    return 0;
+}
