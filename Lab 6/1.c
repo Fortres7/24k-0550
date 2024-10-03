1c21e4fb4b7780c64315fa28744de417cea05a22
Maarij <127932628+Fortres7@users.noreply.github.com>
2024-10-03 14:32:28 +0500

    Create 1.c

---
+++ Lab 6/1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int num, sum=0;
+	
+	while(num!=0){
+	
+			
+		
+	printf("Enter Number: \n");
+	scanf("%d", &num);
+	
+	sum=sum+num;
+	
+	}
+	
+	printf("Sum is: %d\n", sum);
+	
+	
+}
