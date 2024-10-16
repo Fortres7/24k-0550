db940c1eb411d0d93f3440cba8d246ba8c2240c1
Maarij <127932628+Fortres7@users.noreply.github.com>
2024-10-16 11:48:58 +0500

    Create 2.c

---
+++ Lab 6/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+  
+  int is_prime, n, i=2;
+  
+  printf("Enter number: ");
+  scanf("%d", &n);
+  
+  while(i<=n){
+      
+      if(n%i==0 && i!=n && i!=1){
+          
+          is_prime=0;
+          break;
+      }
+      is_prime=1;
+      i++;
+  }
+  
+  if(is_prime){
+      printf("%d is a Prime Number", n);
+  }
+  else {
+  printf("%d is not a Prime Number", n);
+  }
+    return 0;
+}
