a2f896650f4735c20c70f1dffeb4bdd00266d527
Maarij <127932628+Fortres7@users.noreply.github.com>
2024-11-26 18:48:06 +0500

    Update 1.c

--- Lab 11/1.c
+++ Lab 11/1.c
@@ -1,1 +1,31 @@
-asd
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *sourceFile, *destinationFile;
+    char ch;
+
+    sourceFile = fopen("source.txt", "r");
+    if (sourceFile == NULL) {
+        fprintf(stderr, "Could not open source.txt for reading.\n");
+        return 1;
+    }
+
+    destinationFile = fopen("destination.txt", "w");
+    if (destinationFile == NULL) {
+        fprintf(stderr, "Could not open destination.txt for writing.\n");
+        fclose(sourceFile);
+        return 1;
+    }
+
+    while ((ch = fgetc(sourceFile)) != EOF) {
+        fputc(ch, destinationFile);
+    }
+
+    fclose(sourceFile);
+    fclose(destinationFile);
+
+    printf("Contents copied from successfully.\n");
+
+    return 0;
+}
