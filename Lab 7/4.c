9380b14b3a367f9f1d7a767397315981d50c6c56
Maarij <127932628+Fortres7@users.noreply.github.com>
2024-12-02 12:39:05 +0500

    Create 4.c

---
+++ Lab 7/4.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    char *start, *end, temp;
+    int length = 0;
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    char *newline = str;
+    while (*newline != '\0') {
+        if (*newline == '\n') {
+            *newline = '\0';
+            break;
+        }
+        newline++;
+    }
+
+    char *ptr = str;
+    while (*ptr != '\0') {
+        length++;
+        ptr++;
+    }
+    printf("Length of the string: %d\n", length);
+
+    char str2[length];
+
+    for (int i = 0; i < length; i++)
+    {
+      str2[i] = str[i];
+    }
+    
+    start = str;       
+    end = str + length - 1;
+
+    while (start < end) {
+
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    for (int i = 0; i < length; i++)
+    {
+      if (str[i] == str2[i])
+      {
+        count++;
+      }
+      
+    }
+
+    if (count == length)
+    {
+      printf("The String is palindrome");
+    }
+    else{
+      printf("The String is not palindrome");
+    }
+    
+    return 0;
+}
